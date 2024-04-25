ee0118ca44fcf74ecc6b0351936d7f495b58b6f1
hariom <yhariom243@gmail.com>
2024-04-26 03:44:02 +0530

    first commit

---
+++ hotel.sql
@@ -0,0 +1,260 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 26, 2024 at 12:13 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hotel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `fullname` varchar(100) DEFAULT NULL,
+  `phoneno` int(10) DEFAULT NULL,
+  `email` text DEFAULT NULL,
+  `cdate` date DEFAULT NULL,
+  `approval` varchar(12) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`id`, `fullname`, `phoneno`, `email`, `cdate`, `approval`) VALUES
+(2, 'RAO HARIOM YADAV', 2147483647, 'yhariom243@gmail.com', '2024-04-25', 'Allowed'),
+(3, 'hariom yadav', 2147483647, 'yhariom243@gmail.com', '2024-04-26', 'Not Allowed');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `usname` varchar(30) DEFAULT NULL,
+  `pass` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `usname`, `pass`) VALUES
+(1, 'Admin', '1234'),
+(2, 'hero', '1234');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `newsletterlog`
+--
+
+CREATE TABLE `newsletterlog` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(52) DEFAULT NULL,
+  `subject` varchar(100) DEFAULT NULL,
+  `news` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `newsletterlog`
+--
+
+INSERT INTO `newsletterlog` (`id`, `title`, `subject`, `news`) VALUES
+(1, 'demo ', 'jhhjhg', ' mn nbn');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `id` int(11) DEFAULT NULL,
+  `title` varchar(5) DEFAULT NULL,
+  `fname` varchar(30) DEFAULT NULL,
+  `lname` varchar(30) DEFAULT NULL,
+  `troom` varchar(30) DEFAULT NULL,
+  `tbed` varchar(30) DEFAULT NULL,
+  `nroom` int(11) DEFAULT NULL,
+  `cin` date DEFAULT NULL,
+  `cout` date DEFAULT NULL,
+  `ttot` double(8,2) DEFAULT NULL,
+  `fintot` double(8,2) DEFAULT NULL,
+  `mepr` double(8,2) DEFAULT NULL,
+  `meal` varchar(30) DEFAULT NULL,
+  `btot` double(8,2) DEFAULT NULL,
+  `noofdays` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `payment`
+--
+
+INSERT INTO `payment` (`id`, `title`, `fname`, `lname`, `troom`, `tbed`, `nroom`, `cin`, `cout`, `ttot`, `fintot`, `mepr`, `meal`, `btot`, `noofdays`) VALUES
+(2, 'Mr.', 'RAO hariom', 'YADAV', 'Single Room', 'Double', 1, '2024-04-26', '2024-04-27', 150.00, 162.00, 9.00, 'Half Board', 3.00, 1),
+(3, 'Mr.', 'hariom', 'yadav', 'Deluxe Room', 'Double', 1, '2024-04-26', '2024-04-27', 220.00, 237.60, 13.20, 'Half Board', 4.40, 1),
+(4, 'Dr.', 'rrr', 'yyy', 'Deluxe Room', 'Double', 1, '2024-04-26', '2024-04-27', 220.00, 237.60, 13.20, 'Half Board', 4.40, 1),
+(5, 'Mr.', 'demorao', 'ydv', 'Guest House', 'Double', 1, '2024-04-27', '2024-04-28', 180.00, 194.40, 10.80, 'Half Board', 3.60, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room`
+--
+
+CREATE TABLE `room` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `type` varchar(15) DEFAULT NULL,
+  `bedding` varchar(10) DEFAULT NULL,
+  `place` varchar(10) DEFAULT NULL,
+  `cusid` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `room`
+--
+
+INSERT INTO `room` (`id`, `type`, `bedding`, `place`, `cusid`) VALUES
+(2, 'Superior Room', 'Double', 'Free', NULL),
+(3, 'Superior Room', 'Triple', 'Free', NULL),
+(4, 'Single Room', 'Quad', 'Free', NULL),
+(5, 'Superior Room', 'Quad', 'Free', NULL),
+(6, 'Deluxe Room', 'Single', 'Free', NULL),
+(7, 'Deluxe Room', 'Double', 'Free', 0),
+(8, 'Deluxe Room', 'Triple', 'Free', NULL),
+(9, 'Deluxe Room', 'Quad', 'Free', NULL),
+(10, 'Guest House', 'Single', 'Free', NULL),
+(11, 'Guest House', 'Double', 'NotFree', 5),
+(12, 'Guest House', 'Quad', 'Free', NULL),
+(13, 'Single Room', 'Single', 'Free', NULL),
+(14, 'Single Room', 'Double', 'Free', 0),
+(15, 'Single Room', 'Triple', 'Free', NULL),
+(16, 'Guest House', 'Triple', 'Free', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roombook`
+--
+
+CREATE TABLE `roombook` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `Title` varchar(5) DEFAULT NULL,
+  `FName` text DEFAULT NULL,
+  `LName` text DEFAULT NULL,
+  `Email` varchar(50) DEFAULT NULL,
+  `National` varchar(30) DEFAULT NULL,
+  `Country` varchar(30) DEFAULT NULL,
+  `Phone` text DEFAULT NULL,
+  `TRoom` varchar(20) DEFAULT NULL,
+  `Bed` varchar(10) DEFAULT NULL,
+  `NRoom` varchar(2) DEFAULT NULL,
+  `Meal` varchar(15) DEFAULT NULL,
+  `cin` date DEFAULT NULL,
+  `cout` date DEFAULT NULL,
+  `stat` varchar(15) DEFAULT NULL,
+  `nodays` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `roombook`
+--
+
+INSERT INTO `roombook` (`id`, `Title`, `FName`, `LName`, `Email`, `National`, `Country`, `Phone`, `TRoom`, `Bed`, `NRoom`, `Meal`, `cin`, `cout`, `stat`, `nodays`) VALUES
+(3, 'Mr.', 'hariom', 'yadav', 'demo12@gmail.com', 'india', 'India', '987654321', 'Deluxe Room', 'Double', '1', 'Half Board', '2024-04-26', '2024-04-27', 'Conform', 1),
+(5, 'Mr.', 'demorao', 'ydv', 'demo@gmail.com', 'india', 'India', '987654321', 'Guest House', 'Double', '1', 'Half Board', '2024-04-27', '2024-04-28', 'Conform', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `newsletterlog`
+--
+ALTER TABLE `newsletterlog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `roombook`
+--
+ALTER TABLE `roombook`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `newsletterlog`
+--
+ALTER TABLE `newsletterlog`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `room`
+--
+ALTER TABLE `room`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `roombook`
+--
+ALTER TABLE `roombook`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
